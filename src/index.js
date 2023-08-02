0485134afdd5df95eb80618899a74e10614ab978
Natalie <favoritewillow@gmail.com>
2023-08-02 22:30:44 +0300

    feat: add reverse-int (Wednesday 02.08.2023 22:30pm)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  const str = n.toString();
+
+  if (str[0] === "-") {
+      let result = Number(str.slice(1).split("").reverse().join(""));
+      return (result);
+  } else {
+      let result = Number(str.split("").reverse().join(""));
+      return result;
+  }
+
 }
